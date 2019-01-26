eb1d7850f1f32841e894120981acd4282a04e95c
sdim-ua <ingersol@yandex.ua>
2019-01-27 00:37:41 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,25 @@
+/*
+Суммирование
+*/
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        
+         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+       int sum = 0; 
+
+     while (true){
+          int numb1 = Integer.parseInt(reader.readLine());
+          sum = sum + numb1;
+          if (numb1 == -1){
+              break;
+          }
+     }   
+       
+    System.out.println(sum + +1);     
+        
+    }
+}
